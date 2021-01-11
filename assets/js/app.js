9ae6aa3c36ca324a939f3c2894af497c68dfcf10
zervis <zervis@gmail.com>
2021-01-11 15:45:08 +0100

    Before: Persisting Annotations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,22 @@
 // We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
+// The ExtractTextPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import css from "../css/app.scss"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
 //
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
+// Import dependencies
 //
 import "phoenix_html"
+
+// Import local files
+//
+// Local files can be imported directly using relative paths, for example:
+// import socket from "./socket"
+
+import socket from "./socket"
+import Video from "./video"
+
+Video.init(socket, document.getElementById("video"))
--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -107,13 +107,14 @@ defmodule Rumbl.Multimedia do
   def list_user_videos(%Accounts.User{} = user) do
     Video
     |> user_videos_query(user)
-    |> Repo.all()
+    |> Repo.all() 
   end
 
   def get_user_video!(%Accounts.User{} = user, id) do
     Video
     |> user_videos_query(user)
     |> Repo.get!(id)
+    |> Repo.preload(:category)
   end
 
   def user_videos_query(query, %Accounts.User{id: user_id}) do
